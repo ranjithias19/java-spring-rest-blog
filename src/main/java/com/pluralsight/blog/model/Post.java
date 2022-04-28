615958a821f857c29a56675c75d7eb96ac6e5fc8
Ranjith Kumar Cheepirishetti <Ranjith.Kumar@Triconinfotech.com>
2022-04-28 22:50:05 +0530

    completed

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -14,8 +14,7 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Version
-    private Long version;
+    @Version private Long version;
     @NotNull
     @Size(min=4, max=100)
     private String title;
